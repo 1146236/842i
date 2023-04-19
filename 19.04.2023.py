ba195f3ac514cda0f4b3581f817941f5e10ef774
Ramazan Qandaxov <130030249+Ramazan062@users.noreply.github.com>
2023-04-19 17:22:14 +0400

    Update 19.04.2023.py

--- 19.04.2023.py
+++ 19.04.2023.py
@@ -14,3 +14,27 @@ def square(a,b,c):
     return result
 
 print(square(3,4,5))
+
+# Ramazan
+# (c-b*x)**0.5=a*x
+a=int(input("Enter the first index:"))
+b=int(input("Enter the second index:"))
+c=int(input("Enter the third index:"))
+def func (a,b,c):
+    D=b**2-4*a*c
+    if D==0:
+        x=(-b-D**0.5)/2*a
+        result=x
+    if D<0:
+        x="Heqiqi koku yoxdur."
+        result=x
+    if (c-b*x1 or x2)==0:  #Kok altindadi deye
+        x==0
+        result=x
+    else:
+        x1=int((-b-D**0.5)/2*a)
+        x2=int((-b+D**0.5)/2*a)
+        result=x1,x2
+    return result
+
+print(func(a,b,c))
