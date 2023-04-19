4ecb61db78c6c717f9e25e5dd5277d1d7b7cd7d7
Sarkhan Talibzadeh <1146236@mail.ru>
2023-04-19 10:18:50 +0400

    Update 19.04.2023.py

--- 19.04.2023.py
+++ 19.04.2023.py
@@ -1,8 +1,16 @@
 #x^2+2x-3=0
 def square(a,b,c):
-    d=(b*b-4*a*c)**0.5
-    x1=(-b-d)/2*a
-    x2=(-b+d)/2*a
-    return x1,x2
+    d=b*b-4*a*c
+    if(d<0):
+        x="Heqiqi koku yoxdur"  
+        result=x      
+    elif(d==0):
+        x=(-b-d**0.5)/2*a
+        result=x
+    else:
+        x1=(-b-d**0.5)/2*a
+        x2=(-b+d**0.5)/2*a
+        result=x1,x2    
+    return result
 
-print(square(1,2,-3))
+print(square(3,4,5))
