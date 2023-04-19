2c4c1501414daa75833c3a3e8ba901d9ec081e9a
Fidan Tomarova <125898886+fidantomarova@users.noreply.github.com>
2023-04-19 17:08:31 +0400

    Update 19.04.2023.py

--- 19.04.2023.py
+++ 19.04.2023.py
@@ -14,3 +14,25 @@ def square(a,b,c):
     return result
 
 print(square(3,4,5))
+
+#fidan
+#t**2-3*t+1=0
+#differential equations
+def square(a, b, c):
+   d=(b**2-4*a*c)
+   t1=(-b-d**0.5)/2*a
+   t2=(-b+d**0.5)/2*a
+
+   if(d > 0):
+      z = "y == c1*e**({}*x)+c2**({}*x)"
+      print(z.format(t1, t2))
+   elif(d == 0): 
+      z = "y == e**(t*x)*(c1+c2*x)"
+      print(z.format(t1, t2))
+   else:
+      z = "y == e**(k1*x)*(c1*(math.cos(p*x)))+c2*(math.sin(p*x))"
+      print(z.format(t1, t2))
+     
+
+square(2, -3, 1)
+
